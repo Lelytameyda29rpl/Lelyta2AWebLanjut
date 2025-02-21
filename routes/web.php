654b37895fa4a3beb6663396f91c78da6381e9a0
Lelytameyda29rpl <lelyta_meyda_29rpl@student.smktelkom-mlg.sch.id>
2025-02-21 15:14:30 +0700

    routing_jobsheet 2

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,59 @@ Route::get('/', function () {
     return view('welcome'); // Mengembalikan tampilan welcome.blade.php
 });
 
+// Mendefinisikan rute GET dengan URL '/hello'
+Route::get('/hello', function () { 
+    // Mengembalikan tampilan (view) dengan nama 'Hello World'
+    return ('Hello World');  
+});
+
+// Mendefinisikan rute GET dengan URL '/hello'
+Route::get('/world', function () {
+    // Mengembalikan tampilan (view) dengan nama 'World'
+    return 'World';
+   });
+
+// Mendefinisikan rute GET untuk URL utama ('/')
+Route::get('/', function () { 
+    // Mengembalikan teks 'Selamat Datang' sebagai respons langsung
+    return ('Selamat Datang');  
+});
+
+// Mendefinisikan rute GET untuk URL '/about'
+Route::get('/about', function () { 
+    // Mengembalikan teks 'NIM: 2341720124' dan Nama: Lelyta Meyda Ayu Budiyanti sebagai respons langsung
+    return ('NIM: 2341720124 <br> Nama: Lelyta Meyda Ayu Budiyanti'); 
+});
+
+// Mendefinisikan rute GET dengan parameter dinamis {Lelyta}
+Route::get('/user/{name}', function ($name) {
+    // Mengembalikan teks "Nama saya {name}" dengan nilai dari parameter URL
+    return 'Nama saya '.$name;
+    });
+
+// Mendefinisikan rute GET dengan dua parameter dinamis: {post} dan {comment}
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) { 
+    // Mengembalikan teks dengan format "Pos ke-{postId} Komentar ke-{commentId}"
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId; 
+});
+
+// Mendefinisikan rute GET dengan parameter dinamis {id}
+Route::get('/articles/{id} ', function ($id) {
+    // Mengembalikan teks "Halaman Artikel dengan ID {id}" dengan nilai dari parameter URL
+    return 'Halaman Artikel dengan ID '.$id;
+    });
+
+// Mendefinisikan rute GET dengan parameter opsional {name}
+Route::get('/user/{name?}', function ($name = null) { 
+    // Mengembalikan teks "Nama saya {name}", jika parameter kosong akan tetap mencetak "Nama saya "
+    return 'Nama saya '.$name;
+});
+
+// Mendefinisikan rute GET dengan parameter opsional {name}
+Route::get('/user/{name?}', function ($name = 'John') { 
+    // Mengembalikan teks "Nama saya {name}", jika parameter tidak diisi, defaultnya adalah 'John'
+    return 'Nama saya '.$name;
+});
+
 // Route resource untuk ItemController yang secara otomatis membuat route CRUD (index, create, store, show, edit, update, destroy)
 Route::resource('items', ItemController::class);
