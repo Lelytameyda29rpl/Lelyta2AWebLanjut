b5260b74b7e74c18e6fd1229b178126e88e3372a
Lelytameyda29rpl <lelyta_meyda_29rpl@student.smktelkom-mlg.sch.id>
2025-02-22 16:55:15 +0700

    implementasi DB Facade_jobsheet 3

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
+use App\Http\Controllers\LevelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -37,3 +38,6 @@ Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
 
 // Route Penjualan (POS)
 Route::get('/sales', [SalesController::class, 'index']);
+
+// Route LevelController
+Route::get('/level', [LevelController::class, 'index']);
