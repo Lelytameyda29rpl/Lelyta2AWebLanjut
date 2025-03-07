adaa538a8d592837f2d35bcb701f1ea91dea1961
Lelytameyda29rpl <lelyta_meyda_29rpl@student.smktelkom-mlg.sch.id>
2025-03-07 15:06:31 +0700

    Attribute Changes_jobsheet 4

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -173,19 +173,68 @@ class UserController extends Controller
         //}
 
         // Jobsheet 4 Praktikum 2.4 Nomor 10
-        public function index() {
-            $user = UserModel::firstOrNew(
+        //public function index() {
+            //$user = UserModel::firstOrNew(
+                //[
+                    //'username' => 'manager33',
+                    //'nama' => 'Manager Tiga Tiga',
+                    //'password' => Hash::make('12345'),
+                    //'level_id' => 2,
+                //],
+            //);
+            //$user->save();
+
+            //return view('user', ['data' => $user]);
+        //}
+
+        // Jobsheet 4 Praktikum 2.5 Nomor 1
+        //public function index() {
+            //$user = UserModel::create(
+                //[
+                    //'username' => 'manager55',
+                    //'nama' => 'Manager55',
+                    //'password' => Hash::make('12345'),
+                    //'level_id' => 2
+                //],
+            //);
+            //$user->username = 'manager56';
+    
+            //$user->isDirty();
+            //$user->isDirty('username');
+            //$user->isDirty('nama');
+            //$user->isDirty(['nama', 'username']);
+    
+            //$user->isClean();
+            //$user->isClean('username');
+            //$user->isClean('nama');
+            //$user->isClean(['nama', 'username']);
+    
+            //$user->save();
+    
+            //$user->isDirty();
+            //$user->isClean();
+            //dd($user->isDirty());
+        //}
+
+         // Jobsheet 4 Praktikum 2.5 Nomor 3
+         public function index() {
+            $user = UserModel::create(
                 [
-                    'username' => 'manager33',
-                    'nama' => 'Manager Tiga Tiga',
+                    'username' => 'manager11',
+                    'nama' => 'Manager11',
                     'password' => Hash::make('12345'),
                     'level_id' => 2,
-                ],
-            );
+                ]);
+
+            $user->username = 'manager12';
+    
             $user->save();
-            
-            return view('user', ['data' => $user]);
+    
+            $user->wasChanged();
+            $user->wasChanged('username');
+            $user->wasChanged(['username', 'level_id']);
+            $user->wasChanged('nama');
+            dd($user->wasChanged(['nama', 'username']));
         }
-        
 }
 
