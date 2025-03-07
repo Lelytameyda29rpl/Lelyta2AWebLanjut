91bcbd4639cf09f61118da0478703cf339b4d5f7
Lelytameyda29rpl <lelyta_meyda_29rpl@student.smktelkom-mlg.sch.id>
2025-03-07 13:52:31 +0700

    Not Found Exceptions _jobsheet 4

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -92,11 +92,24 @@ class UserController extends Controller
                 //}
 
         // Jobsheet 4 Praktikum 2.1 Nomor 10
+        //public function index() {
+            //$user = UserModel::findOr(20, ['username', 'nama'], function () {
+                //abort(404);
+            //});
+            //return view('user', ['data' => $user]);
+                //}
+
+        // Jobsheet 4 Praktikum 2.2 Nomor 1
+        //public function index() {
+            //$user = UserModel::findOrFail(1);
+            //return view('user', ['data' => $user]);
+            //}
+
+        // Jobsheet 4 Praktikum 2.2 Nomor 3
         public function index() {
-            $user = UserModel::findOr(20, ['username', 'nama'], function () {
-                abort(404);
-            });
+            $user = UserModel::where('username', 'manager9')->firstOrFail();
             return view('user', ['data' => $user]);
-                }
+            }
+
 }
 
